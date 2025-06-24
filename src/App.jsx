be3aef6cd705486053cc7f9f14d0d1a97fa1cd84
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-06-25 01:22:53 +0500

    Feature section started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React from 'react'
 import Navbar from '../src/components/navbar/Navbar.jsx'
 import HeroSection from './components/HeroSection/HeroSection.jsx'
+import Features from './components/Features/Feature.jsx'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="min-h-screen bg-white"> 
       <Navbar />
       <HeroSection />
+      <Features />
       </div>
     </>
   )
