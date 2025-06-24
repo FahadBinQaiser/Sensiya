be70b40da66889ede25885a7cb57d907491e0116
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-06-24 14:17:24 +0500

    Font size changed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <div className="flex items-center space-x-2">
-              <span className="text-xl sm:text-3xl font-bold bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
+              <span className="text-3xl sm:text-4xl bg-gradient-to-r from-indigo-600 to-purple-600 bg-clip-text text-transparent">
                 Sensiya
               </span>
             </div>
