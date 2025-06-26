4d3e88e896b36590c3d840ef044abb53aae58993
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-06-26 17:30:26 +0500

    Changing order of imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import React from 'react'
 import Navbar from '../src/components/navbar/Navbar.jsx'
 import HeroSection from './components/HeroSection/HeroSection.jsx'
-import Pricing from './components/Pricing/Pricing.jsx'
 import Features from './components/Features/Feature.jsx'
 import Tesitmonials from './components/Testimonials/Testimonials.jsx'
+import Pricing from './components/Pricing/Pricing.jsx'
 function App() {
 
   return (
@@ -12,9 +12,9 @@ function App() {
       <div className="min-h-screen bg-white"> 
       <Navbar />
       <HeroSection />
-      <Pricing />
       <Features />
       <Tesitmonials />
+      <Pricing />
       </div>
     </>
   )
