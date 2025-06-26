42078ff6b3bc3e812c897a1593709bfc95c19c81
FahadBinQaiser <fahadbinqaiser3@gmail.com>
2025-06-26 17:24:51 +0500

    Pricing section started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React from 'react'
 import Navbar from '../src/components/navbar/Navbar.jsx'
 import HeroSection from './components/HeroSection/HeroSection.jsx'
+import Pricing from './components/Pricing/Pricing.jsx'
 function App() {
 
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="min-h-screen bg-white"> 
       <Navbar />
       <HeroSection />
+      <Pricing />
       </div>
     </>
   )
